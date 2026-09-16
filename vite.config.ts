66f83fe5826d65f9ebc33be2d1f29f9d1418306e
Menna Abed <abedmena767@gmail.com>
2026-09-16 11:00:19 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    resolve: {
+return {
+  base: '/azurea-resort/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
